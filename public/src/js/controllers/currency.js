f94598b837c2c7c90e5d708477c8860b466af64c
nodecode <info@nodecode.de>
2014-09-02 18:41:46 +0200

    BVC

--- public/src/js/controllers/currency.js
+++ public/src/js/controllers/currency.js
@@ -19,7 +19,7 @@ angular.module('insight.currency').controller('CurrencyController',
 
         if (this.symbol === 'USD') {
           response = _roundFloat((value * this.factor), 2);
-        } else if (this.symbol === 'm') {
+        } else if (this.symbol === 'mBVC') {
           this.factor = 1000;
           response = _roundFloat((value * this.factor), 5);
         } else {
@@ -42,7 +42,7 @@ angular.module('insight.currency').controller('CurrencyController',
         Currency.get({}, function(res) {
           $rootScope.currency.factor = $rootScope.currency.bitstamp = res.data.bitstamp;
         });
-      } else if (currency === 'm') {
+      } else if (currency === 'mBVC') {
         $rootScope.currency.factor = 1000;
       } else {
         $rootScope.currency.factor = 1;
